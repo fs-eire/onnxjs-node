13d63ff915aab64888a3bcd1cb6c4059936be1aa
Yulong Wang <f.s@qq.com>
2019-02-22 17:53:57 -0800

    working...

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const onnx_proto = require('onnx-proto');
 
 // create folder /bin
 if (!fs.existsSync(path.join(__dirname, 'bin'))) {
@@ -7,7 +8,8 @@ if (!fs.existsSync(path.join(__dirname, 'bin'))) {
 }
 
 // copy file onnxruntime.node
-fs.copyFileSync(path.join(__dirname, './build/Release/onnxruntime.node'), path.join(__dirname, 'bin', 'onnxruntime.node'));
+fs.copyFileSync(path.join(__dirname, './build/Debug/onnxruntime.node'), path.join(__dirname, 'bin', 'onnxruntime.node'));
+fs.copyFileSync(path.join(__dirname, './build/Debug/onnxruntime.pdb'), path.join(__dirname, 'bin', 'onnxruntime.pdb'));
 
 // copy DLL
 fs.copyFileSync(path.join(__dirname, './onnxruntime/bin/win-x64/native/mkldnn.dll'), path.join(__dirname, 'bin', 'mkldnn.dll'));
@@ -15,5 +17,12 @@ fs.copyFileSync(path.join(__dirname, './onnxruntime/bin/win-x64/native/onnxrunti
 
 const ox = require('./bin/onnxruntime.node');
 
-console.log(ox);
+const sess = new ox.InferenceSession();
+sess.loadModel('D:\\eire\\onnxjs\\deps\\data\\data\\test\\onnx\\v7\\resnet50\\model.onnx');
 
+const inputTensor0 = onnx_proto.onnx.TensorProto.decode(fs.readFileSync('D:\\eire\\onnxjs\\deps\\data\\data\\test\\onnx\\v7\\resnet50\\test_data_set_0\\input_0.pb'));
+const rawData = new Float32Array(1*3*224*224);
+const rawDataUint8 = new Uint8Array(rawData.buffer, rawData.byteOffset, rawData.length * 4);
+rawDataUint8.set(inputTensor0.rawData);
+
+const result = sess.run([{dims: [1,3,224,224], data: rawData}]);
--- src/inference-session.cc
+++ src/inference-session.cc
@@ -1,3 +1,5 @@
+#include <core/providers/cpu/cpu_provider_factory.h>
+
 #include "inference-session.h"
 
 Napi::FunctionReference InferenceSession::constructor;
@@ -56,7 +58,123 @@ Napi::Value InferenceSession::LoadModel(const Napi::CallbackInfo& info) {
   return env.Undefined();
 }
 
+OrtValue * JavascriptTensorToOrtValue(OrtAllocatorInfo *allocatorInfo, Napi::Env env, Napi::Value val) {
+  if (!val.IsObject()) {
+    Napi::Error::New(env, "tensor must be an object").ThrowAsJavaScriptException();
+  }
+
+  auto tensorObject = val.As<Napi::Object>();
+  auto dimsValue = tensorObject.Get("dims");
+  if (!dimsValue.IsArray()) {
+    Napi::Error::New(env, "tensor.dims must be an array").ThrowAsJavaScriptException();
+  }
+
+  auto dimsArray = dimsValue.As<Napi::Array>();
+  auto len = dimsArray.Length();
+  std::vector<size_t> dims;
+  dims.reserve(len);
+  for (uint32_t i = 0; i < len; i++) {
+    Napi::Value dimValue = dimsArray[i];
+    if (!dimValue.IsNumber()) {
+      Napi::Error::New(env, "tensor.dims must be an array of numbers").ThrowAsJavaScriptException();
+    }
+    auto dimNumber = dimValue.As<Napi::Number>();
+    double dimDouble = dimNumber.DoubleValue();
+    if (floor(dimDouble) != dimDouble || dimDouble < 0 || dimDouble > 4294967295) {
+      Napi::Error::New(env, "invalid dimension").ThrowAsJavaScriptException();
+    }
+    size_t dim = static_cast<size_t>(dimDouble);
+    dims.push_back(dim);
+  }
+
+  auto dataValue = tensorObject.Get("data");
+  if (!dataValue.IsTypedArray()) {
+    Napi::Error::New(env, "tensor.data must be an typed array").ThrowAsJavaScriptException();
+  }
+  auto dataTypedArray = dataValue.As<Napi::TypedArray>();
+  if (napi_float32_array != dataTypedArray.TypedArrayType()) {
+    Napi::Error::New(env, "currently only support FLOAT32 typed array").ThrowAsJavaScriptException();
+  }
+  void * buffer = dataTypedArray.ArrayBuffer().Data();
+  size_t byteOffset = dataTypedArray.ByteOffset();
+  size_t byteLength = dataTypedArray.ByteLength();
+  void * data = static_cast<char *>(buffer) + byteOffset;
+  
+  OrtValue *ortTensorValue;
+  OrtCreateTensorWithDataAsOrtValue(allocatorInfo, data, byteLength, &dims[0], dims.size(), ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT, &ortTensorValue);
+
+  return ortTensorValue;
+}
+
+Napi::Value OrtValueToJavascriptTensor(Napi::Env env, OrtValue *value) {
+  Napi::EscapableHandleScope scope(env);
+  auto obj = Napi::Object::New(env);
+  return scope.Escape(obj);
+}
+
+
 Napi::Value InferenceSession::Run(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  Napi::EscapableHandleScope scope(env);
   // TODO: implement session.run()
+
+
+  OrtAllocatorInfo *allocatorInfo;
+  OrtCreateCpuAllocatorInfo(OrtDeviceAllocator, OrtMemTypeDefault, &allocatorInfo);
+  // TODO: scope guard for `allocatorInfo`
+
+
+  //
+  // info[0]: inputs                 Tensor[]
+  if (info.Length() <= 0) {
+    Napi::TypeError::New(env, "Expect argument: input tensors").ThrowAsJavaScriptException();
+  }
+  if (!info[0].IsArray()) {
+    Napi::TypeError::New(env, "Expect the first argument to be an array of input tensors").ThrowAsJavaScriptException();
+  }
+  auto passedInInputTensors = info[0].As<Napi::Array>();
+  auto passedInInputsCount = passedInInputTensors.Length();
+  std::vector<OrtValue *> inputValues;
+  inputValues.reserve(passedInInputsCount);
+  for (uint32_t i = 0; i < passedInInputsCount; i++) {
+    OrtValue *value = JavascriptTensorToOrtValue(allocatorInfo, env, passedInInputTensors[i]);
+    if (!value) {
+      Napi::TypeError::New(env, "not a valid tensor input").ThrowAsJavaScriptException();
+    }
+    inputValues.push_back(value);
+  }
+
+  OrtAllocator *allocator;
+  auto status = OrtCreateDefaultAllocator(&allocator);
+  // TODO: scope guard for `allocator`
+
+  size_t inputCount;
+  status = OrtSessionGetInputCount(this->session_, &inputCount);
+  size_t outputCount;
+  status = OrtSessionGetOutputCount(this->session_, &outputCount);
+
+  std::vector<const char *> inputNames;
+  std::vector<const char *> outputNames;
+  inputNames.reserve(inputCount);
+  outputNames.reserve(outputCount);
+  for (size_t i = 0; i < inputCount; i++) {
+    char *str;
+    OrtSessionGetInputName(this->session_, i, allocator, &str);
+    inputNames.push_back(str);
+  }
+  for (size_t i = 0; i < outputCount; i++) {
+    char *str;
+    OrtSessionGetOutputName(this->session_, i, allocator, &str);
+    outputNames.push_back(str);
+  }
+
+  std::vector<OrtValue *> outputValues(outputCount);
+
+  status = OrtRun(this->session_, nullptr, &inputNames[0], &inputValues[0], inputCount, &outputNames[0], outputCount, &outputValues[0]);
+
+  for (size_t i = 0; i < outputCount; i++) {
+    Napi::Value tensorObject = OrtValueToJavascriptTensor(env, outputValues[i]);
+  }
+
   return info.Env().Undefined();
 }
